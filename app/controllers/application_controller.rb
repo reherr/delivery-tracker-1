a476977ed3dc5edd75b83f9c40cf55a6ac697cc6
Rebecca Herrera <133678924+reherr@users.noreply.github.com>
2024-06-21 01:01:40 +0000

    making visitors sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   skip_forgery_protection
+  before_action :authenticate_user!
+
+  private
+
+  def authenticate_user_with_alert!
+    unless current_user
+      flash[:alert] = "You need to sign in or sign up before continuing."
+      redirect_to new_user_session_path
+    end
+  end
 end
